5f130618146d8c2e361c1a5a4d02db6fd4691535
Elizbeth <elizabethadebayo269@gmail.com>
2024-07-30 21:06:02 +0100

    refactoring to improve modularity

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let (query, file_path) = parse_config(&args);
     // dbg!(args);
     let query = &args[1];
     let file_path = &args[2];
@@ -11,4 +12,10 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("should have been able to read the file");
     println!("With text:\n{contents}");
+}
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
